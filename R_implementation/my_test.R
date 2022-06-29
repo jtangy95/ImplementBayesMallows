a653d1f568e845d70fd3a592c51135267c93af05
jtangy95 <jtangy95@gmail.com>
2022-06-29 16:45:09 +0900

    slight change of relative path for cpp functions

--- R_implementation/my_test.R
+++ R_implementation/my_test.R
@@ -1,10 +1,14 @@
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_partition_function.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_tidy_mcmc.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_compute_mallows.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_plot.BayesMallows.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_compute_consensus.R")
+## set working directory to use relative path
+setwd("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/R_implementation")
 
-load(file = "/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/dataset_potatoes.RData", verbose = T)
+source("./my_partition_function.R")
+source("./my_tidy_mcmc.R")
+source("./my_compute_mallows.R")
+source("./my_plot.BayesMallows.R")
+source("./my_compute_consensus.R")
+
+
+load(file = "./dataset_potatoes.RData", verbose = T)
 
 
 fit = compute_mallows(potatoes_rank$visual_assess, verbose = F, nmc = 100000)
@@ -24,10 +28,10 @@ fit$burnin = 1000
 plot(fit, parameter = "alpha")
 plot(fit, parameter = "rho", items = c(1,3,5,7,9,11))
 plot(fit, parameter = "rho", items = c("P1", "P10"))
-compute_consensus(fit, parameter = "rho" , type = "CP")
-compute_consensus(fit, parameter = "rho" , type = "MAP")
+# compute_consensus(fit, parameter = "rho" , type = "CP")
+# compute_consensus(fit, parameter = "rho" , type = "MAP")
 
-load(file = "/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/dataset_sushi.RData", verbose = T)
+load(file = "./dataset_sushi.RData", verbose = T)
 
 fit = compute_mallows(sushi_rankings, nmc = 100000, verbose = F)
 fit_is = compute_mallows(sushi_rankings, nmc = 100000, verbose = F, importance_sampling = T)
@@ -50,8 +54,8 @@ compute_consensus(fit_is, parameter = "rho" , type = "CP")
 compute_consensus(fit_is, parameter = "rho" , type = "MAP")
 
 
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_compute_mallows_partial.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_tidy_mcmc_partial.R")
+source("./my_compute_mallows_partial.R")
+source("./my_tidy_mcmc_partial.R")
 library(dplyr)
 
 create_miss = function(rankings, fix = NULL, lambda = NULL){
@@ -77,7 +81,7 @@ potato_miss2 = create_miss(potatoes_rank$visual_assess, lambda = 10)
 fit = compute_mallows_partial(potato_miss1, verbose =F, nmc = 100000, save_aug = T)
 fit$burnin = 1000
 plot(fit, parameter = "rho", items = 1:10)
-compute_consensus(fit, parameter = "rho", type = "CP")
+# compute_consensus(fit, parameter = "rho", type = "CP")
 fit$augmented_data %>% filter(assessor == 3, iteration == 100)
 fit$aug_acceptance
 compute_consensus(fit, parameter = "Rtilde", type = "CP", assessors = 2)
@@ -87,32 +91,32 @@ compute_consensus(fit, parameter = "Rtilde", type = "MAP", assessors = c(2,3))
 
 
 
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_topk.R")
+source("./my_topk.R")
 fit = compute_mallows_partial(potato_miss2, verbose =F, nmc = 100000, save_aug = T)
 fit$burnin = 1000
 predict_top_k(fit, k = 10) %>% filter(assessor ==1)
 predict_top_k(fit, k = 10) %>% filter(assessor ==10)
-compute_consensus(fit, parameter = "rho", type = "CP")
-plot_top_k(fit, k=10)
+# compute_consensus(fit, parameter = "rho", type = "CP")
+# plot_top_k(fit, k=10)
 
 sushi_miss = create_miss(sushi_rankings, fix = 5)
 fit = compute_mallows_partial(sushi_miss, verbose = T, nmc = 10000, save_aug = T, aug_thinning = 10)
 fit$burnin = 1000
 predict_top_k(fit, k = 7) %>% filter(assessor ==1)
 predict_top_k(fit, k = 7) %>% filter(assessor ==10)
-compute_consensus(fit, parameter = "rho", type = "CP")
+# compute_consensus(fit, parameter = "rho", type = "CP")
 
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_compute_mallows_cluster.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_tidy_mcmc_cluster.R")
-source("/Users/changtaeyeong/Desktop/BayesMallowsRankModel/ImplementBayesMallows/Implementation_MallowsRankModel/my_plot.BayesMallows_cluster.R")
+source("./my_compute_mallows_cluster.R")
+source("./my_tidy_mcmc_cluster.R")
+source("./my_plot.BayesMallows_cluster.R")
 fit = compute_mallows_cluster(potato, verbose = T, nmc = 1000, n_clusters = 2)
 fit$rho[,,1]
 fit$rho[,,100]
 fit$rho_acceptance
 fit$alpha_acceptance
-fit$cluster_probs[, 900]
-fit$cluster_assignment[, 10:30]
-fit$cluster_assignment[, 80:100]
+fit$cluster_probs
+fit$cluster_assignment
+fit$cluster_assignment
 
 fit = compute_mallows_cluster(sushi_rankings, nmc = 30000, verbose = T, n_clusters = 3, clus_thin = 10, rho_thinning = 10)
 
